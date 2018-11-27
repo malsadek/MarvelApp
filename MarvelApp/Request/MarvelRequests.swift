b99df65802bee553c0233536359d52ec6c3ca190
Mariam Al Sadek <mariam.alsadek@lau.edu>
2018-11-27 23:57:05 +0200

    Created main view controller - character listing

--- MarvelApp/Request/MarvelRequests.swift
+++ MarvelApp/Request/MarvelRequests.swift
@@ -12,8 +12,10 @@ import CryptoSwift
 class MarvelRequests : NSObject {
     static var sharedInstance =  MarvelRequests()
     let api: String = "https://gateway.marvel.com/"
-    let publicKey: String = "INSERT YOUR MARVEL PUBLIC KEY"
-    let privateKey: String = "INSERT YOUR MARVEL PRIVATE KEY"
+//    let publicKey: String = "INSERT YOUR MARVEL PUBLIC KEY"
+//    let privateKey: String = "INSERT YOUR MARVEL PRIVATE KEY"
+    let publicKey: String = "d71553cd5c809dbc25feac28d1d7c363"
+    let privateKey: String = "5260f0ccd0f1e1572fba13c06a16110bea6836a7"
     let ts = NSDate().timeIntervalSince1970.description
     
     override init(){
@@ -25,7 +27,7 @@ class MarvelRequests : NSObject {
             .setUrl(_url: "https://gateway.marvel.com:443/v1/public/characters?apikey=\(publicKey)&ts=\(ts)&hash=\((ts + privateKey + publicKey).md5())")
             .addParam(name: "offset", value: "\(offset)")
             .setMethod(_method : Request.METHOD_GET)
-            .setParser(parser: BaseParser())
+            .setParser(parser: CharacterParser())
             .prepare();
     }
     
--- MarvelApp/ViewControllers/MainViewController.swift
+++ MarvelApp/ViewControllers/MainViewController.swift
@@ -8,13 +8,79 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: MarvelViewController {
+    
+    @IBOutlet weak var charactersTableView: UITableView!
+    
+    let cellIdentifier = "cell"
+    var marvelCharacters =  [MarvelCharacter]()
+    var count = 0
+    var limit = 0
+    //    var offset = 0
+    var total = 0
+    var spinner: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.charactersTableView.delegate = self
+        self.charactersTableView.dataSource = self
+        self.charactersTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        self.spinner = UIActivityIndicatorView(style: .gray)
+        self.spinner.frame = CGRect(x: 0.0, y: 0.0, width: self.charactersTableView.frame.width, height: 50.0)
+        self.getCharacters()
+        
     }
-
-
+    
+    func getCharacters(){
+        self.charactersTableView.tableFooterView = self.spinner
+        self.spinner.startAnimating()
+        let request = MarvelRequests.sharedInstance.characters(offset: self.count)
+        self.dispatchRequest(
+            request : request,
+            showLoader: (self.count == 0),
+            foreground : {(p, e) -> Void in
+                if e == nil && p.isSuccessful() {
+                    let parser : CharacterParser = p as! CharacterParser ;
+                    self.marvelCharacters.append(contentsOf: parser.getResults())
+                    self.count += parser.getCount()
+                    self.limit = parser.getLimit()
+                    //                    self.offset = parser.getOffset()
+                    self.total = parser.getTotal()
+                    self.spinner.stopAnimating()
+                    self.charactersTableView.tableFooterView = UIView()
+                    self.charactersTableView.reloadData()
+                    
+                }
+        },
+            background : {(p, e) -> Void in
+                
+        });
+    }
 }
 
+//MARK: - Tableview delegate
+extension MainViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.marvelCharacters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        let marvelChar = self.marvelCharacters[indexPath.row]
+        cell.textLabel?.text =  marvelChar.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //present details
+        let marvelChar = self.marvelCharacters[indexPath.row]
+
+    }
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row == (self.marvelCharacters.count - 1) {
+            if self.count < self.total {
+                self.getCharacters()
+            }
+        }
+    }
+}
